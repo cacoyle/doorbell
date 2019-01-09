a041d1128250a001243c020d000e4d3507b95b2a
cacoyle <cacoyle@github.com>
2019-01-09 01:23:31 +0000

    Use proper blocking technique for input.

--- pintest.py
+++ pintest.py
@@ -4,33 +4,26 @@ import time
 
 mylcd = i2c_lcd_driver.lcd()
 
+def dingdong(ctx):
+    print("doorbell pressed")
+    mylcd.lcd_display_string("doorbell pressed", 1)
+
+def clearlcd():
+    mylcd.lcd_clear()
+
 doorbell = 16
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(doorbell, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(doorbell, GPIO.RISING, callback=dingdong, bouncetime=300)
+# GPIO.add_event_detect(doorbell, GPIO.FALLING, callback=clearlcd, bouncetime=300)
 
-last_state = None
-last_change = ""
-last_poll = time.time()
 try:
     while(True):
-        poll_interval = time.time() - last_poll
-        last_poll = time.time()
-        # print(poll_interval)
-        # print(GPIO.input(doorbell))
-        if GPIO.input(doorbell):
-           state = "PRESSED"
-        else:
-           state = "NOT PRESSED"
-    
-        if state != last_state:
-            last_change = time.time()
+        print("Waiting for press...")
+        time.sleep(1)
+        if not GPIO.input(doorbell):
             mylcd.lcd_clear()
-            print("%s: %s" % (state, last_change))
-            mylcd.lcd_display_string(state, 1)
     
-        last_state = state
-        # time.sleep(0.05)
+    
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-
